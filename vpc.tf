367473c35bd20e836771e4eab4da184f9547c6c6
SameepSig <sameepsigdel@gmail.com>
2024-05-23 13:38:05 +0545

    Tags added in VPC

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,11 @@
 resource "aws_vpc" "sameep_terraform_vpc_1" {
   cidr_block = "10.0.0.0/16"
+
+  tags= {
+  Name = "sameep_terraform_vpc_1"
+    silo = "intern2"
+    owner = "sameep.sigdel"
+    terraform = "true"
+    environment = "dev"
+  }
 }
